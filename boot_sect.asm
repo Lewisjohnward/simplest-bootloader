28236b72121ecf0bc6087824e26d43b3d72be273
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2024-03-17 00:52:07 +0000

    add [org 0x7c00]

--- boot_sect.asm
+++ boot_sect.asm
@@ -2,33 +2,14 @@
 ; A simple boot sector program that demonstrates addressing
 ;
 
+[org 0x7c00] ; This removes the need
+             ; to add the offset
+
 mov ah, 0x0e ; int 10/ah = 0eh => scrolling teletype BIOS routine
 
-; First attempt
-mov al, the_secret
-int 0x10            ; Does this print an X?
-                    ; No, loads the offset 
-                    ; but not the character
-
-; Second attempt
-mov al, [the_secret] ; Does this print an X?
-int 0x10             ; No, CPU treats the offset
-                     ; as though it was from
-                     ; the start of memory,
-                     ; rather than start
-                     ; address of loaded code
-
-; Third attempt
-mov bx, the_secret   ; Does this print an X?
-add bx, 0x7c00       ; Yes, memory location
-mov al, [bx]         ; is correct
+mov bx, the_secret
+mov al, [bx]
 int 0x10        
-
-; Fourth attempt
-mov al, [0x7c1e]
-int 0x10             ; Does this print an X?
-                     ; Yes, memory location
-                     ; is correct
 
 jmp $
 
