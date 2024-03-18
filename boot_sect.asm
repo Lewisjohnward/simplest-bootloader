4c8d9a871fa853f9632f7257a0860fa4b60167dc
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2024-03-18 00:23:48 +0000

    working

--- boot_sect.asm
+++ boot_sect.asm
@@ -2,22 +2,34 @@
 ; A simple boot sector program that demonstrates the stack
 ;
 
-[org 0x7c00] ; This removes the need
-             ; to add the offset
+mov ah, 0x0e
 
-mov ah, 0x0e ; int 10/ah = 0eh => scrolling teletype BIOS routine
+mov al, [a]          ; Prints incorrectly
+int 0x10
 
+mov bx, 0x7c0        ; Can't set ds directly
+mov ds, bx           ; so set bx then copy
+mov al, [b]          ; bx to ds
+int 0x10             ; prints b
 
-mov bx, 0x0
-call print_hex
+mov al, [es:c]       ; Prints incorrectly
+int 0x10
+
+mov bx, 0x7c0        ; Prints d
+mov es, bx
+mov al, [es:d]
+int 0x10
 
 jmp $
 
-%include "print_string.asm"
-%include "print_hex.asm"
-
-BOOTING:
-  db "Booting LewOS", 0
+a:
+  db "a"
+b:
+  db "b"
+c:
+  db "c"
+d:
+  db "d"
 
 ; Padding and magic boot sector number
 times 510-($-$$) db 0
